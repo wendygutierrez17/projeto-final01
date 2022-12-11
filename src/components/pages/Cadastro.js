662cb5406e4819417543f163e6841d6b4990933a
wendygutierrez17 <wendygut.17@gmail.com>
2022-12-11 17:52:46 -0300

    atulizando cadastro

--- src/components/pages/Cadastro.js
+++ src/components/pages/Cadastro.js
@@ -1,11 +1,13 @@
 import React from "react"
 import Form from 'react-bootstrap/Form';
+import styles from "./formulario.module.css"
 import Button from 'react-bootstrap/Button';
 
 function Cadastro(){
     return(
             
-        <div><h2>Cadastre-se</h2>
+        <div className={styles.cadastro}>
+            <h2>Cadastre-se</h2>
              <Form>
       <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
         <Form.Label>Nome Completo</Form.Label>
@@ -16,13 +18,11 @@ function Cadastro(){
         <Form.Label>E-mail</Form.Label>
         <Form.Control type="email" placeholder="example@email.com" />
       </Form.Group>
+    
+
+    <Button variant="outline-primary" type= "submit" >Enviar</Button>{' '}
     </Form>
-
-    <Button>
-    <Button variant="outline-primary">Envio</Button>{' '}
-    </Button>
     </div> 
-
          
         
     )
